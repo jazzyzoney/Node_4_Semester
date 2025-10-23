1a285384032bf7f616313ad23ca29e57a48c4111
jazzyzoney <jaqtash22@gmail.com>
2025-10-23 09:50:00 +0000

    failing - make new

--- 07._First_Svelte_Project/src/App.svelte
+++ 07._First_Svelte_Project/src/App.svelte
@@ -1,7 +1,9 @@
 <script>
     import Counter from "./components/counter/Counter.svelte";
+    import Pokemon from "./components/pokemon/Pokemon.svelte";
 
 </script>
 <h1>welcome</h1>
 
 <Counter />
+<Pokemon />
---
+++ 07._First_Svelte_Project/src/components/pokemon/Pokemon.svelte
@@ -0,0 +1,6 @@
+<script>
+    let pokemonName = "Who's that pokemon";
+</script>
+
+<h1>{pokemonName}</h1>
+<button on:click={() => pokemonName = "Charizard"}>It's...</button>
